43dcfd7322a8cecdf8855b28aa3b8f0d7eed105b
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-11-14 15:23:52 +0000

    Making menu bar collapse again on click

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,14 @@
 $(document).ready(function () {
 
     // initiates carousel scrolling
-
     $('.carousel').carousel({
-        interval: 2000
+        interval: 3000
       });
 
-    //   $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-    //     var next = $(this).next();
-    //     if (!next.length) {
-    //       next = $(this).siblings(':first');
-    //     }
-    //     next.children(':first-child').clone().appendTo($(this));
-      
-    //     for (var i=0;i<4;i++) {
-    //       next=next.next();
-    //       if (!next.length) {
-    //         next=$(this).siblings(':first');
-    //       }
-    //       next.children(':first-child').clone().appendTo($(this));
-    //     }
-    //   });
+
+      // closes menu bar on click
+      $(".nav-link").click(function () {
+        $("#navbarToggleExternalContent").removeClass("show");
+        });
 
 });
