d2bb70cd5824cecaefd596cac70f761abece9dbc
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-11-14 12:10:11 +0000

    Improving the responsiveness of the portfolio carousel

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
 
     // initiates carousel scrolling
-    $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-        const next = $(this).next();
-        if (!next.length) {
-          next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-      
-        for (let i = 0; i < 4; i++) {
-          next=next.next();
-          if (!next.length) {
-            next=$(this).siblings(':first');
-          }
-          next.children(':first-child').clone().appendTo($(this));
-        }
+
+    $('.carousel').carousel({
+        interval: 2000
       });
+
+    //   $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+    //     var next = $(this).next();
+    //     if (!next.length) {
+    //       next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+      
+    //     for (var i=0;i<4;i++) {
+    //       next=next.next();
+    //       if (!next.length) {
+    //         next=$(this).siblings(':first');
+    //       }
+    //       next.children(':first-child').clone().appendTo($(this));
+    //     }
+    //   });
 
 });
